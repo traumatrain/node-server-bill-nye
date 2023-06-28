798169733a908aebf701beffca9f66b400daa597
Michael Day <demarkdown@gmail.com>
2023-06-27 21:25:41 -0500

    Create the bootstrap file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const { PORT, IP } = require('./src/config');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, IP, () =>
+  console.log(`API server is running at http://localhost:${PORT}...`)
+);
