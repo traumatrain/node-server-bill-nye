a16029679bdbcdb0a24defa8524253f886901e6e
traumatrain <107575726+traumatrain@users.noreply.github.com>
2023-07-31 15:59:22 -0400

    added more facts to module
    added more facts from online source of bill nye facts to nye-fact-module

--- src/nye-fact-module.js
+++ src/nye-fact-module.js
@@ -9,6 +9,14 @@ const nyeFacts = [
   'Bill himself wrote and produced his show which was filmed entirely in Seattle',
   'After his work as The Science Guy, Bill still kept his interest for teaching science utilizing entertainment. In the 1998 Disney Movie called “The Principal Takes a Holiday,” Bill made a hovercraft while demonstrating scientific applications in a novel classroom setup',
   'From 2000-2002, he was the technical expert of “BattleBots”, and in 2004-2005, he hosted the award winning series for The Science Channel called “100 Greatest Discoveries”',
+  'In 2005, Bill became The Planetary Societys vice president, and this society advocated space research especially on other planets—Mars in particular',
+  'Bill Nye holds several U.S. patents which include ballet pointe shoes and an educational magnifying glass which can be created by filling a plastic bag with some water',
+  'Bill Nye has lived in Los Angeles since 2006',
+  'Bill Nye served as a university professor at Cornell University.',
+  'Nye is also a fellow of the Committee for Skeptical Inquiry',
+  'He has received an honorary doctorate award from the Johns Hopkins University, and an Honorary Doctor of Science degree from Willamette University among other awards',
+  'He is a self-proclaimed lover of swing, and has been spotted having fun in dancing venues in the Los Angeles area.',
+  'He featured in the 13-episode series from PBS KCTS-TV which was called “The Eyes of Nye”.',
 ];
 
 // exports array to index
